01dfc063b7ee5616e1aebeb892d514fd01b9f44c
Liu Yuxuan <cdjddzy@foxmail.com>
2019-07-01 10:08:14 +0800

    init backend code

---
+++ code/backend/online-edu/src/main/java/com/se231/onlineedu/OnlineEduApplication.java
@@ -0,0 +1,13 @@
+package com.se231.onlineedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEduApplication.class, args);
+	}
+
+}
