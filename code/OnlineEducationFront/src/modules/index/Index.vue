bac0757c38079a4abeaf3218cd83fa04e77edb36
zhangyuxin <1069866826@qq.com>
2019-07-03 15:35:03 +0800

    zyx第一次提交，教务员的用户管理和课程管理前端页面。

---
+++ code/OnlineEducationFront/src/modules/index/Index.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div id="index">
+        <div>这是Index.vue</div>
+
+        <a href="manage.html">link to manage</a>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ code/OnlineEducationFront/src/modules/manage/components/ManageUser.vue
@@ -0,0 +1,67 @@
+<template>
+
+    <div>
+        <div>
+            <el-input v-model="search" placeholder="请输入用户名" prefix-icon="el-icon-search"
+                      style="width:50%; padding: 8px; border: none; font-size: 25px; height: 30px"/>
+
+            <el-table :data="UserData.filter(data=>!search || data.username.includes(search))"
+                      style="width: 100%; font-size: 20px; padding-top: 10px; padding-bottom:20px;;"
+                        cell-style="border:1px solid;">
+                <el-table-column >
+                    <el-table-column
+                            type="index"
+                    >
+                    </el-table-column>
+                    <el-table-column
+                            prop="username"
+                            label="用户名"
+                            min-width="25%"
+                            sortable
+                    ></el-table-column>
+                    <el-table-column
+                            prop="password"
+                            label="密码"
+                            min-width="25%"
+                    ></el-table-column>
+                    <el-table-column
+                            prop="email"
+                            label="邮箱"
+                            sortable
+                    ></el-table-column>
+                </el-table-column>
+            </el-table>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "ManageUser",
+
+        data(){
+            return{
+                search: '',
+                UserData: [
+                    {
+                        username:"kamen",
+                        password:"123",
+                        email:"1099@fg.co"
+                    },
+                    {
+                        username:"admin",
+                        password:"ewt",
+                        email:"1daswew9@fger.coq"
+                    }
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
