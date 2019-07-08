24521eacc3c12a2db2cb1cfd8a40ff5df2f42d59
zhangyuxin <1069866826@qq.com>
2019-07-08 17:00:07 +0800

    教师公告页面，修改代码

--- code/OnlineEducationFront/src/modules/index/components/TeacherCourseNav.vue
+++ code/OnlineEducationFront/src/modules/index/components/TeacherCourseNav.vue
@@ -3,48 +3,50 @@
         <div id="TeacherCourseNav" class="nav">
             <el-row class="tac">
                 <el-col :span="16">
-                    <h3>某课程</h3>
+                    <span>
+                        <h3>{{courseTitle}}</h3>
+                    </span>
                     <el-menu
                             class="el-menu-vertical-demo"
                             router="true">
                         <el-menu-item index="1" route="/course/manager/detail">
-                            <i class="el-icon-copy-document"></i>
+                            <i class="el-icon-view"></i>
                             <span slot="title">
                             课程详情
                         </span>
                         </el-menu-item>
                         <el-menu-item index="2" route="/course/manager/student">
-                            <i class="el-icon-copy-document"></i>
+                            <i class="el-icon-user"></i>
                             <span slot="title">
                             选课学生
                         </span>
                         </el-menu-item>
                         <el-menu-item index="3" route="/course/manager/edit">
-                            <i class="el-icon-copy-document"></i>
+                            <i class="el-icon-edit-outline"></i>
                             <span slot="title">
                             课程编辑
                         </span>
                         </el-menu-item>
-                        <el-menu-item index="4" route="/course/manager/annoucement">
-                            <i class="el-icon-copy-document"></i>
+                        <el-menu-item index="4" route="/course/manager/announcement">
+                            <i class="el-icon-message"></i>
                             <span slot="title">
                             课程公告
                         </span>
                         </el-menu-item>
                         <el-menu-item index="5" route="/course/manager/BT">
-                            <i class="el-icon-copy-document"></i>
+                            <i class="el-icon-folder"></i>
                             <span slot="title">
                             课程资源
                         </span>
                         </el-menu-item>
                         <el-menu-item index="6" route="/course/manager/score">
-                            <i class="el-icon-copy-document"></i>
+                            <i class="el-icon-data-analysis"></i>
                             <span slot="title">
                             成绩管理
                         </span>
                         </el-menu-item>
                         <el-menu-item index="7" route="/course/manager/assignment">
-                            <i class="el-icon-copy-document"></i>
+                            <i class="el-icon-notebook-1"></i>
                             <span slot="title">
                             课程作业
                         </span>
@@ -61,7 +63,13 @@
 
 <script>
     export default {
-        name: "TeacherCourseNav"
+        name: "TeacherCourseNav",
+
+        data(){
+            return{
+                courseTitle:"软件工程导论",
+            }
+        }
     }
 </script>
 
