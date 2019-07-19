9ca6996ed8bed6fb0d7cec9d7c68eb4311f52cf2
StarkLyu <41030195+StarkLyu@users.noreply.github.com>
2019-07-19 09:17:12 +0800

    Successfully initial mobile project

---
+++ code/OEMobile/screens/LoginScreen.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Item, Input, Icon, Button , Text} from 'native-base';
+
+export default class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+        this.onLogin = this.onLogin.bind(this);
+    }
+
+    onLogin() {
+        fetch("http://202.120.40.8:30382/online-edu/api/auth/signin",{
+            method: "POST",
+            body: this.state
+        }).then(() => {
+            alert("登录成功！");
+            this.props.navigation.navigate("Home");
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <Item>
+                        <Icon type={'FontAwesome'} name='user' />
+                        <Input placeholder='Icon Textbox' onChangeText={(text) => this.setState({username: text})}/>
+                    </Item>
+                    <Item>
+                        <Input
+                            placeholder='Icon Alignment in Textbox'
+                            onChangeText={(text) => this.setState({password: text})} secureTextEntry = {true}/>
+                        <Icon active name='swap' />
+                    </Item>
+                    <Button full onPress={this.onLogin}>
+                        <Text>登录</Text>
+                    </Button>
+                </Content>
+            </Container>
+        );
+    }
+}
---
+++ code/OnlineEducationFront/src/modules/index/components/CourseHead.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="image-div">
+            <img src="../../../assets/logo.png" class="image-div">
+        </div>
+        <div class="title-div">
+            <h2>{{ courseTitle }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CourseHead",
+        computed: {
+            courseTitle: function () {
+                return this.$store.state.course.courseInfo.courseTitle;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .image-div {
+        width: 300px;
+        height: 185px;
+        background-color: white;
+    }
+
+    .title-div {
+        width: 300px;
+        text-align: center;
+    }
+</style>
