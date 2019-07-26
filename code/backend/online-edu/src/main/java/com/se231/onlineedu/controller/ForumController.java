2bbde71deff25642233209fb19fa5680ab50d37f
Liu Yuxuan <cdjddzy@foxmail.com>
2019-07-26 16:08:27 +0800

    finish forum api

--- code/backend/online-edu/src/main/java/com/se231/onlineedu/controller/ForumController.java
+++ code/backend/online-edu/src/main/java/com/se231/onlineedu/controller/ForumController.java
@@ -5,6 +5,7 @@ import com.se231.onlineedu.model.Forum;
 import com.se231.onlineedu.security.services.UserPrinciple;
 import com.se231.onlineedu.service.*;
 import com.se231.onlineedu.util.SaveFileUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -43,6 +44,7 @@ public class ForumController {
     private int limit = 5120000;
 
 
+    @ApiOperation("上传论坛内容")
     @PostMapping("/courses/{courseId}/sections/{secNo}/forums")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TEACHING_ADMIN','SUPER_ADMIN')")
     public Forum updateForum(@RequestBody Forum forum, @PathVariable Long courseId, @PathVariable int secNo, @AuthenticationPrincipal UserPrinciple userPrinciple) {
@@ -57,23 +59,29 @@ public class ForumController {
         return forumService.updateForum(forum);
     }
 
+    @ApiOperation("获取课程下的所有论坛")
     @GetMapping("/courses/{courseId}/forums")
     public List<Forum> getForumsByCourse(@PathVariable Long courseId){
         return forumService.getForumsByCourse(courseId);
     }
 
+
+    @ApiOperation("获取课程下的的所有论坛")
     @GetMapping("/courses/{courseId}/sections/{secNo}/forums")
     public List<Forum> getForums(@PathVariable Long courseId, @PathVariable int secNo){
         return forumService.getForumsBySection(courseId, secNo);
     }
 
 
+    @ApiOperation("获取section下所有课程")
     @GetMapping("/forums/{id}")
     public Forum getForumById(@PathVariable String id) {
         return forumService.getForum(id);
     }
 
 
+
+    @ApiOperation("为某个论坛上传图片")
     @PostMapping("/forums/{id}/images")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TEACHING_ADMIN','SUPER_ADMIN')")
     public Forum insertImages(@PathVariable String id, @RequestParam("images") MultipartFile[] multipartFiles) throws IOException {
@@ -83,6 +91,7 @@ public class ForumController {
         return forumService.updateForum(forum);
     }
 
+    @ApiOperation("删除某个图片")
     @DeleteMapping("/forums/{id}/images/{filename}")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TEACHING_ADMIN','SUPER_ADMIN')")
     public boolean deleteImage(@PathVariable String id, @PathVariable String filename) throws IOException {
@@ -92,6 +101,7 @@ public class ForumController {
         return SaveFileUtil.deleteImage(filename);
     }
 
+    @ApiOperation("生成词云图")
     @PostMapping("/users/{userId}/courses/{courseId}/forums/")
     public String wordCloud(@PathVariable Long userId, @PathVariable Long courseId) throws IOException {
         List<String> strings = new ArrayList<>();
