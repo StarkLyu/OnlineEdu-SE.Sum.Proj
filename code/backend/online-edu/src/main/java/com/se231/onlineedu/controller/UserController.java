cef2be8a6f6b9eebf644064dd706dad08ee56e5e
Liu Yuxuan <cdjddzy@foxmail.com>
2019-08-31 09:39:44 +0800

    clean imports

--- code/backend/online-edu/src/main/java/com/se231/onlineedu/controller/UserController.java
+++ code/backend/online-edu/src/main/java/com/se231/onlineedu/controller/UserController.java
@@ -20,8 +20,6 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
