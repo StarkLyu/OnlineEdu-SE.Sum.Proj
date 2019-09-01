21cba6d6822ee965d96911d0efa4d41ddf705788
Liu Yuxuan <cdjddzy@foxmail.com>
2019-09-01 12:23:12 +0800

    word cloud

--- code/backend/online-edu/src/main/java/com/se231/onlineedu/serviceimpl/WordCloudServiceImpl.java
+++ code/backend/online-edu/src/main/java/com/se231/onlineedu/serviceimpl/WordCloudServiceImpl.java
@@ -51,7 +51,7 @@ public class WordCloudServiceImpl implements WordCloudService {
         final Dimension dimension = new Dimension(500, 312);
         final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
         wordCloud.setPadding(2);
-        wordCloud.setBackground(new PixelBoundryBackground("src/test/resources/whale_small.png"));
+        wordCloud.setBackground(new PixelBoundryBackground("src/main/resources/whale_small.png"));
         wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
         wordCloud.setFontScalar(new LinearFontScalar(10, 40));
         wordCloud.build(wordFrequencies);
